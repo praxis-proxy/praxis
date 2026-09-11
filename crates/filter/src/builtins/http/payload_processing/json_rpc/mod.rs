f0f6514825a991ba514e972a1c9a1d69af8db851
Shane Utt <shaneutt@linux.com>
2026-09-11 16:20:16 -0400

    Apply suggestion from @shaneutt
    Signed-off-by: Shane Utt <shaneutt@linux.com>

--- crates/filter/src/builtins/http/payload_processing/json_rpc/mod.rs
+++ crates/filter/src/builtins/http/payload_processing/json_rpc/mod.rs
@@ -288,7 +288,11 @@ match http::header::HeaderName::from_bytes(name.as_bytes()) {
     Ok(header_name) => headers_to_remove.push(header_name),
     Err(_) => warn!(header = %name, "cannot strip promotion header: invalid header name"),
 }
-            headers_to_remove.push(header_name);
+        {
+match http::header::HeaderName::from_bytes(name.as_bytes()) {
+    Ok(header_name) => headers_to_remove.push(header_name),
+    Err(_) => warn!(header = %name, "cannot strip promotion header: invalid header name"),
+        }
         }
     }
 }
